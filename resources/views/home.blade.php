c2c74ed75e6640bde7bf38870b6fc733bae68fd1
Jasczlv <ianis.cozlov@gmail.com>
2024-05-25 15:37:15 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('content')
+<section>
+    <div>
+        <h1>Home Page</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi consequuntur sunt labore aliquam dolorum ab. Omnis magni fugiat, doloribus adipisci eos harum odit ullam explicabo eaque, eligendi possimus alias aperiam! In, esse! Veniam totam magnam officia aspernatur. Quidem, a fugiat.
+        </p>
+    </div>
+</section>
